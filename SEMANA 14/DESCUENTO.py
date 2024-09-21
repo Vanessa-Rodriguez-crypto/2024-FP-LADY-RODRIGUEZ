f90e1a50898d0f3f285b3ca9b0b0606de787b18f
LADY RODRIGUEZ <lv.rodriguezc@uea.edu.ec>
2024-09-21 11:34:52 -0500

    SEMANA 14

--- SEMANA 14/DESCUENTO.py
+++ SEMANA 14/DESCUENTO.py
@@ -16,7 +16,7 @@ def calcular_descuento(monto_compra, porcentaje_descuento=10):
 # Programa principal
 
 # Primera llamada con el monto de la compra solamente (10% de descuento por defecto)
-monto1 = 100.00  # Ejemplo de monto de compra
+monto1 = 150.00  # Ejemplo de monto de compra
 descuento1 = calcular_descuento(monto1)
 monto_final1 = monto1 - descuento1
 print(f"Monto de la compra: ${monto1:.2f}")
@@ -24,8 +24,8 @@ print(f"Descuento aplicado: ${descuento1:.2f}")
 print(f"Monto final a pagar: ${monto_final1:.2f}")
 print()
 
-# Segunda llamada con el monto de la compra y el porcentaje de descuento especificado (por ejemplo, 15%)
-monto2 = 200.00  # Otro ejemplo de monto de compra
+# Segunda llamada con el monto de la c  ompra y el porcentaje de descuento especificado (por ejemplo, 15%)
+monto2 = 300.00  # Otro ejemplo de monto de compra
 porcentaje_descuento2 = 15  # Descuento del 15%
 descuento2 = calcular_descuento(monto2, porcentaje_descuento2)
 monto_final2 = monto2 - descuento2
